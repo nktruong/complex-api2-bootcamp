29f0791df73ce7c74d13dc363577195e78b8441b
nktruong <nathankhaitruong@gmail.com>
2021-10-01 12:19:10 -0400

    Redid to an app that makes QR codes with advice

--- js/main.js
+++ js/main.js
@@ -1,43 +1,19 @@
-document.querySelector('button').addEventListener('click', findDayInfo)
+document.querySelector('button').addEventListener('click', makeQuoteMeme)
 
-function findDayInfo(){
-    // get location (zip, city, etc.) and then input into Weather API parameter
-    let location = document.querySelector('input').value
-
-    const url = `https://api.weatherapi.com/v1/current.json?key=0cc54de2b0e14a0ca06154843213009&q=${location}&aqi=yes`
-    // make a fetch request to the Weather API
-    fetch(url)
-        .then(res => res.text())
+function makeQuoteMeme(){
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
         .then(data => {
-            // parse JSON into object so I can access it. Then get latitude + longitude
-            const objectData = JSON.parse(data)
-            let lat = objectData.location.lat
-            let lon = objectData.location.lon
-            let city = objectData.location.name
-            let state = objectData.location.region
-            let country = objectData.location.country
-            let humidity = objectData.current.humidity
-            let tempF = objectData.current.temp_f
-            let tempC = objectData.current.temp_c
+            document.querySelector('span').innerText = data.slip.advice
+            let advice = data.slip.advice
 
-            document.querySelector('#location').innerHTML = `${city}, ${state} in ${country}`
-            document.querySelector('#humidity').innerHTML = humidity
-            document.querySelector('#temperature').innerHTML = `${tempF}°F and ${tempC}°C`
-            console.log(objectData)
+            let url = `https://image-charts.com/chart?chl=${advice}&choe=UTF-8&chs=200x200&cht=qr&icqrf=5D576B`
 
-            // make a fetch request to the sunset API using lat + lon
-            const url2 = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}`
-
-            fetch(url2)
-                .then(res => res.json())
-                .then(data2 => {
-                    document.querySelector('#dayLength').innerText = `${data2.results.day_length} hours`
-                    document.querySelector('#sunrise').innerText = data2.results.sunrise
-                    document.querySelector('#solarNoon').innerText = data2.results.solar_noon
-                    document.querySelector('#sunset').innerText =  data2.results.sunset
-                    console.log(data2)
-                })
+            fetch(url)
+                .then(
+                    document.querySelector('img').src = url
+                )
                 .catch(err => console.log(err))
         })
-        .catch(error => console.log(error)) 
+        .catch(err => console.log(err))
 }
